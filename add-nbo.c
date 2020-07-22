743cea2ea3691df29f976f1cc2b6f45560598082
a2sembly <lsh970804@gmail.com>
2020-07-22 15:46:48 +0900

    Update add-nbo.c

--- add-nbo.c
+++ add-nbo.c
@@ -16,17 +16,18 @@ int main(int argc, char* argv[])
         return printf("Usage: %s <File1> <File2>\n", argv[0]);
 
     fp1 = fopen(argv[1],"rb");
-	fp2 = fopen(argv[2],"rb");
+    fp2 = fopen(argv[2],"rb");
 	
-	fread(&File1, sizeof(uint32_t), 1, fp1);
-	fread(&File2, sizeof(uint32_t), 1, fp2);
+    fread(&File1, sizeof(uint32_t), 1, fp1);
+    fread(&File2, sizeof(uint32_t), 1, fp2);
 	
-	File1 = ConvertNetByteOrder(File1);
-	File2 = ConvertNetByteOrder(File2);
-	Result = File1+File2;
+    File1 = ConvertNetByteOrder(File1);
+    File2 = ConvertNetByteOrder(File2);
+    Result = File1+File2;
 	
-	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", File1, File1, File2, File2, Result, Result);
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", File1, File1, File2, File2, Result, Result);
 
-
+    fclose(fp1);
+    fclose(fp2);
     return 0;
 }
